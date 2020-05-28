f06ceabd5f39dcd8868d1a5192854372793f79aa
Alex Jenkinson <alex.jenkinson@devel-ops.com>
2020-05-28 14:12:44 +0100

    Added parameters and test data

--- class/Appointments.php
+++ class/Appointments.php
@@ -2,21 +2,50 @@
 namespace appointmentSystem;
 
 class Appointments {
-    public function addAppointment();
+    static function viewAllMyAppointments(){
+        // Search DB for appointment
 
-    public function cancelAppointment();
+        $appointments = [
+            ["Health & Wellbeing", "15th March 2020 12:00", "Students Union - Room 1"],
+            ["Academic Support Tutor", "20th March 2020 11:00", "Robert Blackburn - Room 211"],
+        ];
 
-    public function addNoteToAppointment();
+        return $appointments;
+    }
 
-    public function acceptAppointment();
+    public function addAppointment($appointment_id, $start_time, $end_time, $for, $with) {
 
-    public function requestTimeChangeForAppointment();
+    }
 
-    public function checkForOverlappingAppointments();
+    public function cancelAppointment($appointment_id) {
 
-    public function referAppointmentToOtherStaff();
+    }
 
-    public function countCancelledAppointments();
+    public function addNoteToAppointment($appointment_id, $added_note) {
+
+    }
+
+    public function acceptAppointment($appointment_id) {
+
+    }
+
+    public function requestTimeChangeForAppointment($appointment_id, $time_change) {
+
+    }
+
+    public function checkForOverlappingAppointments($appointment_1, $appointment_2) {
+        if($appointment_1 == $appointment_2){
+            return true;
+        }
+    }
+
+    public function referAppointmentToOtherStaff($appointment_id, $staff_to_refer) {
+
+    }
+
+    public function countCancelledAppointments() {
+        // loop through all appointments, count cancelled
+    }
 }
 
 ?>
