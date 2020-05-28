2efb51ec36cd25532ffd5a44700233690d11fe94
Alex Jenkinson <alex.jenkinson@devel-ops.com>
2020-05-28 14:31:36 +0100

    Added template SQL quote

--- class/Appointments.php
+++ class/Appointments.php
@@ -2,8 +2,8 @@
 namespace appointmentSystem;
 
 class Appointments {
-    static function viewAllMyAppointments(){
-        // Search DB for appointment
+    static function viewAllMyAppointments($user_id){
+        // SELECT * FROM appointments WHERE for_id = ?
 
         $appointments = [
             ["Health & Wellbeing", "15th March 2020 12:00", "Students Union - Room 1"],
@@ -13,34 +13,44 @@ class Appointments {
         return $appointments;
     }
 
-    public function addAppointment($appointment_id, $start_time, $end_time, $for, $with) {
-
+    public function addAppointment($date, $start_time, $for, $with) {
+        if(!$this->checkForOverlappingAppointments($start_time)){
+            //INSERT INTO appointments (date, start_time, for, with) VALUES (?, ?, ?, ?)
+        }
+        else {
+            return false;
+        }
     }
 
     public function cancelAppointment($appointment_id) {
-
+        // UPDATE appointments SET cancelled = true WHERE appointment_id = ?
     }
 
     public function addNoteToAppointment($appointment_id, $added_note) {
-
+        // INSERT INTO appointment_notes (appointment_id, note) VALUES (?, ?)
     }
 
     public function acceptAppointment($appointment_id) {
 
     }
 
-    public function requestTimeChangeForAppointment($appointment_id, $time_change) {
-
+    public function requestTimeChangeForAppointment($time_change, $appointment_id) {
+        // UPDATE appointments SET start_time = ? WHERE id = ?
     }
 
-    public function checkForOverlappingAppointments($appointment_1, $appointment_2) {
-        if($appointment_1 == $appointment_2){
+    public function checkForOverlappingAppointments($appointment_1, $appointment_2 = null) {
+        if($appointment_1 == $appointment_2 && $appointment_2 != null){
             return true;
         }
+        
+        // TODO: Check all appointments for user
+        // Loop through and check start time for both
+
+        return false;
     }
 
-    public function referAppointmentToOtherStaff($appointment_id, $staff_to_refer) {
-
+    public function referAppointmentToOtherStaff($staff_to_refer, $appointment_id) {
+        // UPDATE appointments SET with = ? WHERE id = ?
     }
 
     public function countCancelledAppointments() {
