dd03a2d026732ed2115089dc2e6cb811a05de6aa
Alex Jenkinson <alex.jenkinson@devel-ops.com>
2020-05-28 14:13:11 +0100

    Added parameters, test data and some code

--- class/User.php
+++ class/User.php
@@ -1,28 +1,48 @@
 <?php 
 namespace appointmentSystem;
 
+session_start();
+
 class User {
     public $role;
+    
+    public $fake_db = array("id" => 1, "username" => "291039", "password" => '$2y$10$et8e3T.vK33s9pEHuv0fm.EPp4MqkpDxNAWjkZ7micYs3lINAYXO2', "role" => "staff");
 
     public function login($username, $password){
+    
+        //sql_query = "SELECT * FROM users WHERE username = ?";
 
+        if($this->fake_db['username'] == $username){
+            if(password_verify($password, $this->fake_db['password'])){
+                $_SESSION["logged_in"] = true;
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public function verifyLoggedIn(){
-        return ($tmp) ? true : false;
-        
+        return ($_SESSION["logged_in"]) ? true : false;
     }
 
     public function logout(){
-
+        $_SESSION["logged_in"] = false;
     }
 
-    public function changePassword(){
+    public function changePassword($user_id, $old_password, $new_password){
 
+        if(password_verify($old_password, $this->fake_db['password'])){
+            $password = password_hash($new_password, PASSWORD_DEFAULT);
+            // UPDATE users SET password = ?
+        }
     }
 
+    /*
+    TODO: Has lack of any way to authenticate user per spec. Maybe just reset to a basic password, or request they go to IT and verify identity?
+    */
     public function forgotPassword(){
-
+        
     }
 
     public function resetPassword(){
@@ -38,6 +58,12 @@ class Availability extends User{
     public function getAvailability(){
         
     }
+}
+
+class SeniorManagement extends User {
+    /*
+    TODO: Will host all call functions to retrieve data for SM.
+    */
 }
 
 ?>
